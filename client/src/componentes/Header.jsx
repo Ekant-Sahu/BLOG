fd97ee32cd931e017bbabf2879d572afd973c8f9
Ekant-Sahu <61657506+Ekant-Sahu@users.noreply.github.com>
2024-05-17 21:42:12 +0530

    sign up ui desgin

--- client/src/componentes/Header.jsx
+++ client/src/componentes/Header.jsx
@@ -54,6 +54,9 @@ export default function Header() {
         <li>
           <a href="/post" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Posts</a>
         </li>
+        <li>
+          <a href="/sign-up" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">SIGN UP</a>
+        </li>
       </ul>
     </div>
   </div>
