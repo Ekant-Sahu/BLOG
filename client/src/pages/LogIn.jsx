98dfcd7116599d5a3eb14b23ef266d94816db758
Ekant-Sahu <61657506+Ekant-Sahu@users.noreply.github.com>
2024-05-18 22:25:56 +0530

    changed api to login

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -20,7 +20,7 @@ export default function LogIn() {
     try{
       setLoading(true);
       setErrorMessage(null);
-      const res = await fetch('/api/auth/signup',{
+      const res = await fetch('/api/auth/login',{
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(formData),
