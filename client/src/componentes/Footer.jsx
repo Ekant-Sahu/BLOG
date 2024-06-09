5d16cb7fd4456b859678092c81015a417f39203a
Ekant-Sahu <61657506+Ekant-Sahu@users.noreply.github.com>
2024-06-09 19:37:36 +0530

    logo change

--- client/src/componentes/Footer.jsx
+++ client/src/componentes/Footer.jsx
@@ -5,10 +5,10 @@ export default function Footer() {
     <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-          </a>
+        <a href="/home" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src="https://raw.githubusercontent.com/Ekant-Sahu/mysite/main/My%20site/20220914_212716_0000.png" className="h-8" alt="Logo" />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">BLOG</span>
+        </a>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
               <a href="/about" className="hover:underline me-4 md:me-6">About</a>
@@ -25,7 +25,7 @@ export default function Footer() {
           </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/home" className="hover:underline">Ekant</a>. All Rights Reserved.</span>
       </div>
     </footer>
   )
